fc8b638a3e0406eee45dbb1299ae174abe018bce
Marc Bachmann <marc.brookman@gmail.com>
2017-01-31 11:51:19 +0100

    Use boolean-expression module instead of inlining the code

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-new-func, no-useless-escape */
 var safeRegexPattern = /[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g
 var isExpression = /(AND|OR|!|&|\|)/
+var booleanExpression = require('boolean-expression')
 
 module.exports = function getValidate (expression) {
   if (Array.isArray(expression)) expression = expression.join(' ')
@@ -24,27 +25,3 @@ module.exports = function getValidate (expression) {
     '  return ' + checkArray,
     '}'].join('\n'))()
 }
-
-var allOperators = /(!|&&| AND | OR | NOT |\|\||\(|\)| )/g
-function booleanExpression (exp) {
-  exp = exp.split(allOperators).filter(Boolean).reduce(rewrite, [])
-  return function toString (map) {
-    return exp.map(function (t, i, exp) {
-      if (t.type === 'operator') return t.value
-      return (map || expressionToString)(t.value, i)
-    }).join(' ')
-  }
-}
-
-var nativeOperators = /^(!|&&|\|\||\(|\))$/
-var operatorMap = {OR: '||', AND: '&&', NOT: '!'}
-function rewrite (ex, el, i, all) {
-  var t = el.trim()
-  if (!t) return ex
-  if (operatorMap[t]) t = operatorMap[t]
-  if (nativeOperators.test(t)) ex.push({type: 'operator', value: t})
-  else ex.push({type: 'token', value: t.replace(/['\\]/g, '\\$&')})
-  return ex
-}
-
-function expressionToString (token) { return token }
