d93fcaa081e873fa1db4ab92290e106139140d85
Marc Bachmann <marc.brookman@gmail.com>
2023-04-24 14:08:14 +0200

    chore: Migrate to const

--- test.js
+++ test.js
@@ -1,13 +1,15 @@
-var assert = require('assert')
+const assert = require('assert')
 
 // Test validation method
-test(false, false)
-test(false, true)
-test(true, false)
-test(true, true)
+test(false, 'string')
+test(false, 'array')
+test(false, 'set')
+test(true, 'string')
+test(true, 'array')
+test(true, 'set')
 
 // Test scopes that are contained in expression
-var scope = require('./index')
+const scope = require('./index')
 assert.deepEqual(scope('foo*qux bar test quz').scopes, ['foo*qux', 'bar', 'test', 'quz'])
 assert.deepEqual(scope('NOT first AND second').scopes, ['first', 'second'])
 assert.deepEqual(
@@ -15,26 +17,26 @@ assert.deepEqual(
   ['first', 'second', 'third', 'fourth']
 )
 
-function test (mandatoryAsArray, scopeAsArray) {
+function test (mandatoryAsArray, scopeAs = 'string') {
   function scopes (mandatory) {
-    var mod = require('./index')
-    var check
+    const mod = require('./index')
+    let check
     if (mandatoryAsArray) check = mod(mandatory.split(' '))
     else check = mod(mandatory)
 
     return function (val) {
-      if (scopeAsArray) return check(val.split(' '))
+      if (scopeAs === 'array') return check(val.split(' '))
       else return check(val)
     }
   }
 
-  var contains = scopes('foo')
+  const contains = scopes('foo')
   assert.equal(contains('foo bar test qux quz'), true)
   assert.equal(contains('bar test qux quz foo'), true)
   assert.equal(contains('bar test foo qux quz'), true)
   assert.equal(contains('test test foo qux quz'), true)
 
-  var doesNotContain = scopes('foo')
+  const doesNotContain = scopes('foo')
   assert.equal(doesNotContain('bar test qux quz'), false)
   assert.equal(doesNotContain('foobar test qux quz'), false)
   assert.equal(doesNotContain('bar test qux quzfoo'), false)
@@ -47,37 +49,37 @@ function test (mandatoryAsArray, scopeAsArray) {
   assert.equal(doesNotContain('foo😄qux bar test quz'), false)
   assert.equal(doesNotContain('foo,foo bar test quz'), false)
 
-  var multimatch = scopes('first second third')
+  const multimatch = scopes('first second third')
   assert.equal(multimatch('this must match multiple scopes'), false)
   assert.equal(multimatch('not only the first and second'), false)
   assert.equal(multimatch('all of them, the first second and third'), true)
 
-  var mandatoryScopeAsString = scopes('foo bar')
+  const mandatoryScopeAsString = scopes('foo bar')
   assert.equal(mandatoryScopeAsString('foo test qux quz'), false)
   assert.equal(mandatoryScopeAsString('foobar test qux quz'), false)
   assert.equal(mandatoryScopeAsString('bar test quz'), false)
   assert.equal(mandatoryScopeAsString('foo test bar quz'), true)
 
-  var caseSensitive = scopes('foo')
+  const caseSensitive = scopes('foo')
   assert.equal(caseSensitive('foo'), true)
   assert.equal(caseSensitive('FOO'), false)
   assert.equal(caseSensitive('Foo'), false)
 
   // Boolean expressions
-  var expression1 = scopes('first && second && !third')
+  const expression1 = scopes('first && second && !third')
   assert.equal(expression1('foo'), false)
   assert.equal(expression1('first'), false)
   assert.equal(expression1('first second'), true)
   assert.equal(expression1('first second another'), true)
   assert.equal(expression1('first second another third'), false)
 
-  var expression2 = scopes('first || second')
+  const expression2 = scopes('first || second')
   assert.equal(expression2('foo'), false)
   assert.equal(expression2('first'), true)
   assert.equal(expression2('foo second'), true)
   assert.equal(expression2('first second'), true)
 
-  var expression3 = scopes('!first || (second && third && !fourth)')
+  const expression3 = scopes('!first || (second && third && !fourth)')
   assert.equal(expression3('foo'), true)
   assert.equal(expression3('first'), false)
   assert.equal(expression3('first second'), false)
@@ -85,26 +87,25 @@ function test (mandatoryAsArray, scopeAsArray) {
   assert.equal(expression3('second third'), true)
   assert.equal(expression3('first second third foo fourth'), false)
 
-  var expression4 = scopes('NOT first AND second')
+  const expression4 = scopes('NOT first AND second')
   assert.equal(expression4('foo'), false)
   assert.equal(expression4('first'), false)
   assert.equal(expression4('foo first'), false)
   assert.equal(expression4('second'), true)
   assert.equal(expression4('second third'), true)
 
-  var expression5 = scopes('NOT (first AND second)')
+  const expression5 = scopes('NOT (first AND second)')
   assert.equal(expression5('foo'), true)
   assert.equal(expression5('first'), true)
   assert.equal(expression5('second'), true)
   assert.equal(expression5('first second'), false)
   assert.equal(expression5('first second foo'), false)
 
-  var ORkeyword = scopes('first OR second')
+  const ORkeyword = scopes('first OR second')
   assert.equal(ORkeyword('foo bar'), false)
   assert.equal(ORkeyword('first foo bar'), true)
   assert.equal(ORkeyword('second foo bar'), true)
 
-  var m = mandatoryAsArray ? 'array' : 'string'
-  var s = scopeAsArray ? 'array' : 'string'
-  console.log(`Tests executed, mandatory as ${m}, scopes as ${s}`)
+  const m = mandatoryAsArray ? 'array' : 'string'
+  console.log(`Tests executed, mandatory as ${m}, scopes as ${scopeAs}`)
 }
